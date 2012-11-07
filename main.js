d1266737157fbbe198e0a974cd8441dc1844e351
Shrikant Sharat <shrikantsharat.k@gmail.com>
2012-11-07 16:59:21 +0530

    Moved site files from master to gh-pages.

---
+++ main.js
@@ -0,0 +1,16 @@
+(function (w, d) {
+
+    w.WebFontConfig = {
+        google: { families: [ 'Cagliostro', 'Open Sans', 'Inconsolata' ] }
+    };
+
+    var wf = d.createElement('script');
+    wf.src = ('https:' == d.location.protocol ? 'https' : 'http') +
+        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+
+    var s = d.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+
+})(window, document);
