dde0a790ef57284c5904c507a4207b3abb675d20
Shrikant Sharat <shrikantsharat.k@gmail.com>
2013-04-24 10:52:24 +0530

    Startin up the python port.

--- ti.py
+++ ti.py
@@ -1,111 +1,80 @@
-#!/usr/bin/env python
 # encoding: utf-8
 
+"""
+ti is a simple and extensible time tracker for the command line.
+
+Usage:
+  ti on <project-name> [<start-time>...]
+  ti fin [<start-time>...]
+  ti status
+  ti tag <tag>...
+  ti note
+  ti edit
+  ti -h | --help
+  ti version | --version
+
+Options:
+  <start-time>...   A time specification (Described further down).
+  -h --help     Show this help.
+"""
+
 from __future__ import print_function
+from __future__ import unicode_literals
 
-import os, os.path as p
-from collections import namedtuple as nt
-import re
-import datetime as dt
+from docopt import docopt
+import json
+from os import path
+import sys
 
-Activity = nt('Activity', 'name on_time fin_time tags notes')
+class JsonStore(object):
 
-def get_sheet_content(sheet_file):
+    def __init__(self, filename):
+        self.filename = filename
 
-    lines = []
+    def load(self):
 
-    with open(sheet_file) as f:
-        for line in f:
-            line = line[:-1]
-            lines.append('' if line.isspace() else line)
+        if path.exists(self.filename):
+            with open(self.filename) as f:
+                data = json.load(f)
 
-    return '\n'.join(lines)
-
-def parse_time(timestr):
-    return dt.datetime.strptime(timestr,
-            '%a %b %d %H:%M:%S %Z %Y')
-
-def parse_activity(content):
-    lines = content.splitlines()
-    project_line = lines[0].strip()
-
-    match = re.match(r'^ (.+) \s+ \[(.+)\] \s to \s \[(.+)\] (.*) $',
-            project_line, re.X)
-
-    if match is None:
-        return None
-
-    groups = match.groups()
-    project_name = groups[0]
-
-    on_time = parse_time(groups[1])
-    fin_time = parse_time(groups[2])
-
-    tags = [t.lstrip('#') for t in groups[3].strip().split()]
-    notes = [l.strip() for l in lines[1:]]
-
-    activity = Activity(project_name, on_time, fin_time, tags, notes)
-
-    return activity
-
-def read_activities(sheet_file):
-    return [parse_activity(s.strip()) for s in
-            get_sheet_content(sheet_file).split('\n\n')
-            if s]
-
-def age(from_date, since_date=None, target_tz=None, include_seconds=False):
-    tdelta = (since_date or dt.now(target_tz)) - from_date
-    return delta_to_age(tdelta)
-
-def delta_to_age(tdelta, include_seconds=False):
-    seconds = int(round(abs(tdelta.days * 86400 + tdelta.seconds)))
-    return seconds_to_words(seconds)
-
-def seconds_to_words(seconds, include_seconds=False):
-    '''Returns the age as a string
-    Taken from http://www.siafoo.net/snippet/89'''
-
-    distance_in_minutes = int(round(seconds/60))
-
-    if distance_in_minutes <= 1:
-        if include_seconds:
-            for remainder in [5, 10, 20]:
-                if seconds < remainder:
-                    return "less than %s seconds" % remainder
-            if seconds < 40:
-                return "half a minute"
-            elif seconds < 60:
-                return "less than a minute"
-            else:
-                return "1 minute"
         else:
-            if distance_in_minutes == 0:
-                return "less than a minute"
-            else:
-                return "1 minute"
-    elif distance_in_minutes < 45:
-        return "%s minutes" % distance_in_minutes
-    elif distance_in_minutes < 90:
-        return "about 1 hour"
-    elif distance_in_minutes < 1440:
-        return "about %d hours" % (round(distance_in_minutes / 60.0))
-    elif distance_in_minutes < 2880:
-        return "1 day"
-    elif distance_in_minutes < 43220:
-        return "%d days" % (round(distance_in_minutes / 1440))
-    elif distance_in_minutes < 86400:
-        return "about 1 month"
-    elif distance_in_minutes < 525600:
-        return "%d months" % (round(distance_in_minutes / 43200))
-    elif distance_in_minutes < 1051200:
-        return "about 1 year"
-    else:
-        return "over %d years" % (round(distance_in_minutes / 525600))
+            data = {'work': []}
+
+        return data
+
+    def dump(self, data):
+        with open(self.filename, 'w') as f:
+            json.dump(data, f)
+
+
+def action_on(args):
+    data = store.load()
+    work = data['work']
+
+    if work and 'end' not in work[-1]:
+        print('You are already working on ' + work[-1]['name'] +
+                '. Stop it or use a different sheet.', file=sys.stderr)
+        raise SystemExit(1)
+
+    entry = {
+        'name': args['<project-name>'],
+        'start': 'now',
+    }
+
+    work.append(entry)
+    store.dump(data)
+
+    print('Start working on ' + entry['name'] + '.')
+
 
 def main():
-    sheet_file = p.expanduser(os.getenv('SHEET_FILE', '~/.ti-sheet'))
-    activities = read_activities(sheet_file)
-    print(activities)
+    args = docopt(__doc__, version='2.0-alpha')
+
+    if args['on']:
+        action_on(args)
+
+
+store = JsonStore('sheet')
 
 if __name__ == '__main__':
     main()
