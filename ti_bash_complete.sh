9e2b6d635faca9716f703ca24a2d889dc8dc97c5
Tristan Trim <TristanTrim@gmail.com>
2016-05-23 14:08:03 -0300

    Bash autocomplete of tasks. Implemented 'ti list' which lists all tasks.

---
+++ ti_bash_complete.sh
@@ -0,0 +1,18 @@
+
+## To use copy this file into /etc/bash_completion.d/
+## or whatever makes sense on your system.
+
+_ti()
+{
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts=$(ti list)
+
+    if [[ "on o" =~ ${prev} ]] ; then
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        return 0
+    fi
+}
+complete -F _ti ti
